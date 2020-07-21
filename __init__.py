ef04825262ec7eb40cd094c61ea89ca0ddc93af4
KILR <65610641+KILR007@users.noreply.github.com>
2020-07-21 16:02:06 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -24,6 +24,8 @@ SOFTWARE.
 import requests
 dat = None
 smth = None
+class PlayerNotFound(Exception):
+    """ No Player Found!"""
 
 class Players:
     def __init__(self,username):
@@ -32,71 +34,57 @@ class Players:
         f = requests.get(url)
         global dat
         dat = f.json()
+        if "Id" not in dat:
+            Error = PlayerNotFound("Player Not Found!")
+            raise Error
     def id(self):
         global dat
-        if "Id" not in dat:
-            return "User Not Found"
-        else:
-            e = dat["Id"]
-            return e
+        e = dat["Id"]
+        return e
+
     def description(self):
         global dat
-        if "Id" not in dat:
-            return "User Not Found"
+        e = dat["Id"]
+        url = f"https://users.roblox.com/v1/users/{e}"
+        nothing = requests.get(url).json()
+        desc = nothing["description"]
+        if desc == "":
+            return "*No Description*"
         else:
-            e = dat["Id"]
-            url = f"https://users.roblox.com/v1/users/{e}"
-            nothing = requests.get(url).json()
-            desc = nothing["description"]
-            if desc == "":
-                return "*No Description*"
-            else:
-                return desc
+            return desc
     def avatar(self):
         global dat
-        if "Id" not in dat:
-            return "User Not Found"
-        else:
-            e = dat["Id"]
-            return f"https://www.roblox.com/bust-thumbnail/image?userId={e}&width=100&height=100&format=png"
+        e = dat["Id"]
+        return f"https://www.roblox.com/bust-thumbnail/image?userId={e}&width=100&height=100&format=png"
     def created_at(self):
         global dat
-        if "Id" not in dat:
-            return "User Not Found"
+        e = dat["Id"]
+        url = f"https://users.roblox.com/v1/users/{e}"
+        nothingd = requests.get(url).json()
+        c = nothingd["created"]
+        if c == "":
+            return None
         else:
-            e = dat["Id"]
-            url = f"https://users.roblox.com/v1/users/{e}"
-            nothingd = requests.get(url).json()
-            c = nothingd["created"]
-            if c == "":
-                return None
-            else:
-                return c
+            return c
+
     def badges(self):
         global dat
-        if "Id" not in dat:
-            return "User Not Found"
+        f = dat["Id"]
+        url = f"https://www.roblox.com/badges/roblox?userId={f}"
+        mm = requests.get(url).json()
+        if "RobloxBadges" not in mm:
+            return "*No Roblox Badges*"
         else:
-            f = dat["Id"]
-            url = f"https://www.roblox.com/badges/roblox?userId={f}"
-            mm = requests.get(url).json()
-            if "RobloxBadges" not in mm:
+            string = ""
+            for item in mm["RobloxBadges"]:
+                string += f'{item["Name"]} ,'
+            if string == "":
                 return "*No Roblox Badges*"
-            else:
-                string = ""
-                for item in mm["RobloxBadges"]:
-                    string += f'{item["Name"]} ,'
-                if string == "":
-                    return "*No Roblox Badges*"
-                return string
+            return string
     def url(self):
         global dat
-        if "Id" not in dat:
-            return "User Not Found"
-        else:
-            f = dat["Id"]
-            return f"https://www.roblox.com/users/{f}/profile"
-
+        f = dat["Id"]
+        return f"https://www.roblox.com/users/{f}/profile"
 
 
 
