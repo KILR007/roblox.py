b97a82a2275327949a7b3736037d889f541c26f3
KILR <65610641+KILR007@users.noreply.github.com>
2020-08-04 19:08:28 +0530

    Update __init__.py

--- pyrblx/__init__.py
+++ pyrblx/__init__.py
@@ -1,12 +1,12 @@
 
-from catalog import Catalog
-from group import Groups
-from user import Players
+from .catalog import Catalog
+from .group import Groups
+from .user import Players
 __title__ = 'pyrblx'
-__version__ = '2.1'
+__version__ = '2.4'
 __author__ = 'KILR'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2016 by KILR'
+__copyright__ = 'Copyright 2020 by KILR007 (KILR)'
 
 
 
