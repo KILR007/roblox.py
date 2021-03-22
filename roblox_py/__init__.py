7c5a7547ec1b6f775276b550006156c497f24803
KILR <65610641+KILR007@users.noreply.github.com>
2021-03-22 07:52:08 +0000

    license fix

--- roblox_py/__init__.py
+++ roblox_py/__init__.py
@@ -14,5 +14,5 @@ from .Twocaptcha import TwoCaptcha
 __title__ = 'roblox_py'
 __author__ = 'KILR'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2020 KILR'
+__copyright__ = 'Copyright 2021 KILR'
 __version__ = '0.2.4'
