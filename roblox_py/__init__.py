f85a4907cb7c5d30dbcb75970377de864d15d270
KILR <65610641+KILR007@users.noreply.github.com>
2021-04-28 14:44:17 +0530

    added local docs

--- roblox_py/__init__.py
+++ roblox_py/__init__.py
@@ -1,3 +1,21 @@
+"""
+<h1>
+	<strong>roblox.py</strong>
+</h1>
+<p>
+	<a href="https://discord.gg/vpEv3HJ">
+		<img src="https://img.shields.io/discord/591914197219016707.svg?label=Discord&amp;logo=Discord&amp;colorB=7289da&amp;style=for-the-badge" alt="Support Server" />
+	</a>
+	<a href="https://github.com/KILR007/pyrblx/blob/master/LICENSE.txt">
+		<img src="https://img.shields.io/badge/License-MIT-blue.svg" alt="MIT license" />
+	</a>
+	<a href="https://static.pepy.tech/badge/roblox.py">
+		<img src="https://static.pepy.tech/badge/roblox.py" alt="Downloads" />
+	</a>
+	<br />
+	<strong>Modern async API wrapper for Roblox with game client support</strong>
+</p>
+"""
 
 from .exceptions import *
 from .GamepassInfo import GamepassInfo
